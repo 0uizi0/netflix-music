f1eed147bfb2f05cadd98579fda2b8d9fa57a872
0uizi0 <0uizi0@naver.com>
2023-03-03 16:43:53 +0900

    메인 모달 반응형 및 css 복구

--- js/modal.js
+++ js/modal.js
@@ -47,7 +47,7 @@ for(let i=0; i<btnModal.length; i++){
         const infosub = document.querySelector(".info-sub");
         infosub.textContent = infosubText;
 
-        detailTrack.textContent="";
+        // detailTrack.textContent="";
         progress.style.width = "0%";
 
 
